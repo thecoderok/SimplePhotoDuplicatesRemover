d56f10e713e44c5d9ed1886712aa3c634e5914d3
Vitalii Ganzha <vitaliiy@outlook.com>
2020-05-17 17:17:18 -0700

    initial commit

--- SimplePhotoDedup/Form1.Designer.cs
+++ SimplePhotoDedup/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimplePhotoDedup
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.WebsiteLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CurrentlyProcessingLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusText,
+            this.StatusLabel,
+            this.ProgressBar,
+            this.CurrentlyProcessingLabel,
+            this.WebsiteLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(793, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // WebsiteLabel
+            // 
+            this.WebsiteLabel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.WebsiteLabel.IsLink = true;
+            this.WebsiteLabel.Name = "WebsiteLabel";
+            this.WebsiteLabel.Size = new System.Drawing.Size(558, 17);
+            this.WebsiteLabel.Spring = true;
+            this.WebsiteLabel.Text = "https://github.com/thecoderok";
+            this.WebsiteLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.WebsiteLabel.Click += new System.EventHandler(this.WebsiteLabel_Click);
+            // 
+            // StatusText
+            // 
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(45, 17);
+            this.StatusText.Text = "Status: ";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(42, 17);
+            this.StatusLabel.Text = "<Idle>";
+            // 
+            // CurrentlyProcessingLabel
+            // 
+            this.CurrentlyProcessingLabel.Name = "CurrentlyProcessingLabel";
+            this.CurrentlyProcessingLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(793, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.Text = "Simple Photo Deduplication";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel WebsiteLabel;
+        private System.Windows.Forms.ToolStripStatusLabel StatusText;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel CurrentlyProcessingLabel;
     }
 }
 
--- SimplePhotoDedup/Form1.cs
+++ SimplePhotoDedup/Form1.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SimplePhotoDedup
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        const string WebSite = "https://github.com/thecoderok";
+        public MainForm()
         {
             InitializeComponent();
+            this.WebsiteLabel.Text = WebSite;
+        }
+
+        private void WebsiteLabel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(WebSite);
         }
     }
 }
